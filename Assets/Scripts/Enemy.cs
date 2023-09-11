1dcc91135a800e24d3a15c42bb5368322e893ae2
Doğaç Mengül <dogacmengul@yahoo.com>
2023-09-11 17:56:58 +0300

    New enemies and some tweaks

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,31 +8,43 @@ namespace Project_Invasion
     public class Enemy : MonoBehaviour
     {
         [SerializeField] private GameObject enemyDeathEffect;
-        [SerializeField] private Transform parentForSpawned;
         [SerializeField] private int enemyHitScore = 5;
+        [SerializeField] private int enemyHitPoint = 15;
 
         private Score _score;
+        private Rigidbody _enemyRigidBody;
+        private Transform _parentForSpawned;
 
         private void Start()
         {
             _score = FindObjectOfType<Score>();
+            _enemyRigidBody = gameObject.AddComponent<Rigidbody>();
+            _enemyRigidBody.useGravity = false;
+            _parentForSpawned = GameObject.FindGameObjectWithTag("Spawn").transform;
         }
         
         private void OnParticleCollision(GameObject other)
         {
             ProcessHit();
-            ProcessKill();
         }
         
         private void ProcessHit()
         {
+            GameObject vfx = Instantiate(enemyDeathEffect, transform.position, Quaternion.identity);
+            vfx.transform.parent = _parentForSpawned;
             _score.AddScore(enemyHitScore);
+            enemyHitPoint -= enemyHitScore;
+
+            if (enemyHitPoint == 0)
+            {
+                ProcessKill();
+            }
         }
 
         private void ProcessKill()
         {
             GameObject vfx = Instantiate(enemyDeathEffect, transform.position, Quaternion.identity);
-            vfx.transform.parent = parentForSpawned;
+            vfx.transform.parent = _parentForSpawned;
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,16 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace Project_Invasion
 {
     public class Score : MonoBehaviour
     {
         private int _totalPoints;
+        private TMP_Text _scoreText;
 
+        private void Start()
+        {
+            _scoreText = GetComponent<TMP_Text>();
+            _scoreText.text = "Start!";
+        }
+        
         public void AddScore(int pointsToIncrease)
         {
             _totalPoints += pointsToIncrease;
+            _scoreText.text = _totalPoints.ToString();
         }
     }
 }
